58873831b2a2e004048b2f08c68ef0bd286bb4d0
Pedro Henrique <omegapro345@gmail.com>
2024-10-21 21:33:49 -0300

    a

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,10 @@ DevDataSource.initialize().then()
 console.log("Database connected")
 
 const app = express()
+const cors = require('cors');
 
 app.use(express.json())
+app.use(cors({origin: "http://localhost:3000"}))
 app.use(router)
 
 app.listen(3333, () => console.log("Servidor online na porta 3333"))
