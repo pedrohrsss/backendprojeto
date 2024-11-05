995f4c2c52c22986bc4692e83f1d268d1bd57d70
Pedro Henrique <omegapro345@gmail.com>
2024-11-04 21:20:36 -0300

    a

--- src/connections/dbDev.ts
+++ src/connections/dbDev.ts
@@ -3,10 +3,13 @@ import { Task } from "../models/task";
 
 export const DevDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "dpg-csg2a0t6l47c739lsql0-a.oregon-postgres.render.com",
     port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "backend",
+    username: "tasks_db_kyd9_user",
+    password: "JRBFtawPafCBCPx41mPPQO7ifpdHHv78",
+    database: "tasks_db_kyd9",
+    extra: {
+        options: "-c statement_timeout=30000ms -c search_path=monsters"
+    },
     entities: [Task]
 })
